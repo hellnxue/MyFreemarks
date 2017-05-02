cf5ceede605dd8274ddf6ad05c252c52ba019784
hellnxue <1032755304@qq.com>
2017-05-02 19:42:14 +0800

    vue框架学习

--- Vue.js框架/vues/src/components/emit.vue
+++ Vue.js框架/vues/src/components/emit.vue
@@ -1,6 +1,9 @@
 <template>
   <div class='cmpt'>
-    <h4 style='background:yellow;'>子组件</h4>
+    <!--具名Slot-->
+    <slot name='header'></slot>
+
+    <h4 style='background:yellow;' id="cmptTest">子组件</h4>
     <h4>{{ msg }}</h4>
     <div >
       <button type='button' @click='funs()'>点击给父组件传值</button>
@@ -16,7 +19,31 @@
 
        <p> {{variable}}</p>
       <button type='button' @click='dymProp()'>prop的属性在子组件中能不能改</button>
+
+
+      <button type='button' @click='fwffn()'  >子组件访问父组件的元素</button>
+
+      <p class='myColor'>scope作用域中的样式测试-子组件</p>
     </fieldset>
+      <!--单个Slot-->
+      <slot>
+        只有在没有要分发的内容时才会显示。
+      </slot>
+
+      <!--具名Slot-->
+      <slot name='footer'></slot>
+
+      
+      <!--作用域插槽-->
+      <slot text='slot from child'></slot>
+
+
+      <ul>
+        <slot name="item" v-for="item in items" :text="item.text">
+          <!-- 这里写入备用内容 -->
+        </slot>
+      </ul>
+
 
   </div>
 </template>
@@ -29,7 +56,12 @@ export default {
   data () {
     return {
       msg: '子组件给父组件传值$emit',
-      variable:''
+      variable:'',
+      items:[
+        {text:'hello a'},
+        {text:'hello b'},
+        {text:'hello c'}
+      ]
     }
   },
   components:{
@@ -46,6 +78,10 @@ export default {
          //vm3.ptMsg='update ptMsg value'  //!! prop属性的传值是单向的，只能父传子，子组件中不能修改奥~~~
          vm3.variable=vm3.ptMsg;
 
+      },
+      fwffn:function(){
+        var text=document.getElementById('fwf').innerText;
+        console.log(text);
       }
   },
   //通过props属性，让父组件给子组件传值
